fcaa71b0eea716e4d3b0063642f317ee677853eb
Alethia Quintero <alethiaq22@gmail.com>
2019-07-07 21:36:56 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -4,10 +4,10 @@ def run_guessing_game
   guess = gets.chomp 
   if guess == num 
     puts "You guessed the correct number!"
-  end
-  if guess == "exit"
+  
+  elsif  guess == "exit"
     puts "Goodbye!"
-  end
 else 
   puts "Sorry! The computer guessed #{num}"
+end
 end
