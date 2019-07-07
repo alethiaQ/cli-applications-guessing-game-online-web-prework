d9e438ea73ebebb748bd4b008ed41436c6d1546e
Alethia Quintero <alethiaq22@gmail.com>
2019-07-07 21:34:49 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -2,7 +2,7 @@ def run_guessing_game
   num = rand(6)+1
   puts "Guess a number between 1 and 6"
   guess = gets.chomp 
-  
- 
+  if guess == num 
+    puts "You guessed the correct number!"
   
 end
